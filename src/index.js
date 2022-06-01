4639d3a110766027d012e67a12bd998b963c9b29
kenizaya <harveylstark@gmail.com>
2022-06-01 17:48:41 +0530

    Implement createProject and create Task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import {taskit} from './taskit';
 import './styles/main.css';
+import {createTask, getTask, completeTask, changePriority, createProject} from './taskit';
 
 console.log("Hello 123");
-taskit();
+
+console.log(getTask());
+
+createTask("t3", "d3", "dd3", "l");
+
+console.log("after: ", getTask());
+
+changePriority(0, "Low");
+createProject("website blog");
+
+completeTask(1);
+
+
--- src/taskit.js
+++ src/taskit.js
@@ -1,3 +1,33 @@
-export function taskit() {
-    console.log("TASKIT");
+const projects = {
+    "inbox": [
+        {
+            title: "Task Title",
+            description: "Task description",
+            dueDate: "Task date",
+            priority: "High",
+        },
+        {
+            title: "Task Title2",
+            description: "Task description2",
+            dueDate: "Task date2",
+            priority: "High2",
+        }],
+};
+
+const completedTasks = [];
+
+export const createProject = (project) => {
+    projects[project] = [];
+        
 }
+
+export const createTask = (project, title, description, dueDate, priority) => {
+    const task = {};
+    task.title= title;
+    task.description= description;
+    task.dueDate= dueDate;
+    task.priority= priority;
+    console.log(task, "from createTask");
+    projects[project].push(task);
+}
+
